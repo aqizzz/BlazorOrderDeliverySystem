94beaa4048d9b5ef6cadf2acb69a1b2b1d4b0a93
aqizzz <aqizzz@u.circlecat.org>
2024-09-25 11:27:38 -0400

    Create ViewUserProfile ViewMerchantProfile ViewWorkerProfile pages

---
+++ OrderDeliverySystem.Client/Infrastructure/Services/Profile/IProfileService.cs
@@ -0,0 +1,11 @@
+﻿using OrderDeliverySystem.Share.Data;
+using OrderDeliverySystem.Share.DTOs;
+
+namespace OrderDeliverySystem.Client.Infrastructure.Services.Profile
+{
+    public interface IProfileService
+    {
+        Task<Result<UserProfileDTO>> GetCustomer(string id);
+        Task<Result> UpdateCustomer(UserProfileDTO model);
+    }
+}
