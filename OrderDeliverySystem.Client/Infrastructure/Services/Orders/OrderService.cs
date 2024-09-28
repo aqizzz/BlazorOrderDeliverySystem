f8c85c2a1023445861c2bd61cbf035b7207d25ea
aqizzz <aqizzz@u.circlecat.org>
2024-09-28 18:19:33 -0400

    Modify order service

--- OrderDeliverySystem.Client/Infrastructure/Services/Orders/OrderService.cs
+++ OrderDeliverySystem.Client/Infrastructure/Services/Orders/OrderService.cs
@@ -14,69 +14,64 @@ namespace OrderDeliverySystem.Client.Infrastructure.Services.Orders
 {
     public class OrderService
     {
-        private readonly HttpClient _httpClient;
+        private readonly ILocalStorageService localStorage;
+        private readonly AuthenticationStateProvider authenticationStateProvider;
+        private readonly IHttpClientFactory httpClientFactory;
+        private readonly TokenHelper tokenHelper;
+
         private const string Base = "api/Orders/";
 
-        public OrderService(HttpClient httpClient)
+        public OrderService(
+            ILocalStorageService localStorage,
+            AuthenticationStateProvider authenticationStateProvider,
+            IHttpClientFactory httpClientFactory,
+            TokenHelper tokenHelper)
         {
-            _httpClient = httpClient;
+            this.localStorage = localStorage;
+            this.authenticationStateProvider = authenticationStateProvider;
+            this.httpClientFactory = httpClientFactory;
+            this.tokenHelper = tokenHelper;
         }
         public async Task<List<OrderDTO>> GetOrdersByRoleAsync(string role, int id, bool recent)
         {
+            var httpClient = this.httpClientFactory.CreateClient("API");
+            await tokenHelper.ConfigureHttpClientAuthorization(httpClient);
             var uri = $"{Base}{role}/{id}?recent={recent.ToString().ToLower()}";
-            Console.WriteLine($"making request to { uri}");
-			var orders = await _httpClient.GetFromJsonAsync<List<OrderDTO>>(uri);
+
+			var orders = await httpClient.GetFromJsonAsync<List<OrderDTO>>(uri);
 			return orders ?? new List<OrderDTO>();
 		}
 
         public async Task<List<OrderDTO>> GetOrdersTableByRoleAsync(string role, int id, bool recent )
         {
+            var httpClient = this.httpClientFactory.CreateClient("API");
+
+            await tokenHelper.ConfigureHttpClientAuthorization(httpClient);
             var uri = $"{Base}/table/{role}/{id}?recent={recent.ToString().ToLower()}";
-            Console.WriteLine($"making request to {uri}");
-            var orders = await _httpClient.GetFromJsonAsync<List<OrderDTO>>(uri);
+            var orders = await httpClient.GetFromJsonAsync<List<OrderDTO>>(uri);
             return orders ?? new List<OrderDTO>();
         }
 
         public async Task<Result> CreateOrderAsync(CreateOrderDTO order)
         {
+            var httpClient = this.httpClientFactory.CreateClient("API");
+
+            await tokenHelper.ConfigureHttpClientAuthorization(httpClient);
             var uri = $"{Base}/create";
-            Console.WriteLine($"Base Address: {_httpClient.BaseAddress}");
-            Console.WriteLine($"making request to {uri}");
 
-            try
-            {
-                // Sending the request with payload
-                var response = await _httpClient.PostAsJsonAsync(uri, order);
+            // Sending the request with payload
+            var response = await httpClient.PostAsJsonAsync(uri, order);
 
 
-                if (!response.IsSuccessStatusCode)
-                {
-                    var errors = await response.Content.ReadFromJsonAsync<string[]>();
-                    return errors != null
-                        ? Result.Failure(errors) // Return the errors if present
-                        : Result.Failure(new[] { "An unknown error occurred." });
-                }
+            if (!response.IsSuccessStatusCode)
+            {
+                var errors = await response.Content.ReadFromJsonAsync<string[]>();
+                return errors != null
+                    ? Result.Failure(errors) // Return the errors if present
+                    : Result.Failure(new[] { "An unknown error occurred." });
+            }
 
-                return Result.Success;
-            }
-            catch (HttpRequestException ex)
-            {
-                // Handle network-related exceptions
-                Console.WriteLine($"Network error: {ex.Message}");
-                return Result.Failure(new[] { ex.Message });
-            }
-            catch (TaskCanceledException ex)
-            {
-                // Handle request timeout or cancellations
-                Console.WriteLine($"Request timeout: {ex.Message}");
-                return Result.Failure(new[] { ex.Message });
-            }
-            catch (Exception ex)
-            {
-                // Handle general exceptions
-                Console.WriteLine($"Exception occurred: {ex.Message}");
-                return Result.Failure(new[] { ex.Message });
-            }
+            return Result.Success;
         }
 
         public async Task<HttpResponseMessage> UpdateOrderAsync(OrderDTO order)
@@ -91,9 +86,12 @@ namespace OrderDeliverySystem.Client.Infrastructure.Services.Orders
                     order.Status = "Delivered"; break;
             }
 
+            var httpClient = this.httpClientFactory.CreateClient("API");
+
+            await tokenHelper.ConfigureHttpClientAuthorization(httpClient);
+
             var uri = $"{Base}update/{order.OrderId}";
-            Console.WriteLine($"making request to {uri}");
-            var response = await _httpClient.PutAsJsonAsync(uri, order);
+            var response = await httpClient.PutAsJsonAsync(uri, order);
             return response;
         }
     }
