9ca937c1d4a7d63a81c6d5b91be923fa9a08e651
aqizzz <aqizzz@u.circlecat.org>
2024-10-02 10:29:32 -0400

    Add upload

---
+++ OrderDeliverySystem/Controllers/BlobController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderDeliverySystem.Middleware;
+
+namespace OrderDeliverySystem.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlobController : ControllerBase
+    {
+        private readonly BlobService _blobService;
+
+        public BlobController(BlobService blobService)
+        {
+            _blobService = blobService;
+        }
+
+        [HttpGet("GetSasUrl")]
+        public IActionResult GetSasUrl(string fileName)
+        {
+            var sasUrl = _blobService.GetSasTokenForBlob(fileName);
+            return Ok(sasUrl);
+        }
+    }
+}
---
+++ OrderDeliverySystem/Middleware/BlobService.cs
@@ -0,0 +1,27 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Sas;
+
+namespace OrderDeliverySystem.Middleware
+{
+    public class BlobService
+    {
+        private readonly string _blobStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=shuting;AccountKey=TuucCZnM09Temqt0lWRfbE8Z1Pj4UXfJ9ZSWBQMdZZveLyz9yiXNSlPfuGb88auJE/V/fRMXqSb1+AStkuuw0A==;EndpointSuffix=core.windows.net";
+        private readonly string _containerName = "images";
+
+        public string GetSasTokenForBlob(string blobName)
+        {
+            var blobServiceClient = new BlobServiceClient(_blobStorageConnectionString);
+            var containerClient = blobServiceClient.GetBlobContainerClient(_containerName);
+
+            // Ensure container exists
+            containerClient.CreateIfNotExists();
+
+            var blobClient = containerClient.GetBlobClient(blobName);
+
+            // Generate a SAS Token valid for a limited time
+            var sasToken = blobClient.GenerateSasUri(BlobSasPermissions.Write | BlobSasPermissions.Read, DateTimeOffset.UtcNow.AddHours(1));
+
+            return sasToken.ToString();
+        }
+    }
+}
